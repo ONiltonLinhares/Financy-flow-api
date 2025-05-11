df3c0e94d3d75aef11eb9cf297e96613af3df0c6
ONiltonLinhares <nilton.linhares.n@gmail.com>
2025-05-10 23:05:50 -0300

    first commit

---
+++ FinancyFlow/src/test/java/com/simpleapps/FinancyFlow/FinancyFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleapps.FinancyFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancyFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
